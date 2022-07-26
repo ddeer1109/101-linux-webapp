d39994ce0b15b8ff7a9e3ad8da16fa26ab5873be
Daniel Wator <daniel_wator@epam.com>
2022-07-26 11:50:09 +0200

    2nd commit with sketch

---
+++ sketch.ps1
@@ -0,0 +1,5 @@
+GET https://dev.azure.com/{organization}/{project}/_apis/sourceProviders/{providerName}/filecontents?serviceEndpointId={serviceEndpointId}&repository={repository}&commitOrBranch={commitOrBranch}&path={path}&api-version=5.0-preview.1
+
+GET https://dev.azure.com/Daniel23/azure-mentoring/_apis/sourceProviders/git/filecontents?repository=azure-mentoring&commitOrBranch=master&path=101-basic-linux-webapp.json&api-version=5.0-preview.1
+
+GET https://dev.azure.com/Daniel23/azure-mentoring/_apis/sourceProviders/git/repositories/filecontents?api-version=5.0-preview.1        
